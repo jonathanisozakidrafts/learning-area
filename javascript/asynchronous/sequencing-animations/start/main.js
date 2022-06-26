b37a80cb47a0e32a3d67ca741b385063123474f4
jonathanisozakidrafts <105454180+jonathanisozakidrafts@users.noreply.github.com>
2022-06-26 10:55:58 -0700

    This change uses a Promise chain.

--- javascript/asynchronous/sequencing-animations/start/main.js
+++ javascript/asynchronous/sequencing-animations/start/main.js
@@ -1,14 +1,26 @@
 const aliceTumbling = [
-  { transform: 'rotate(0) scale(1)' },
-  { transform: 'rotate(360deg) scale(0)' }
+    { transform: 'rotate(0) scale(1)' },
+    { transform: 'rotate(360deg) scale(0)' }
 ];
 
 const aliceTiming = {
-  duration: 2000,
-  iterations: 1,
-  fill: 'forwards'
+    duration: 2000,
+    iterations: 1,
+    fill: 'forwards'
 }
 
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
+
+const alice1Promise = alice1.animate(aliceTumbling, aliceTiming).finished;
+
+alice1Promise.then(function(response) {
+
+    return alice2.animate(aliceTumbling, aliceTiming).finished;
+
+}).then(function(response) {
+
+    return alice3.animate(aliceTumbling, aliceTiming).finished;
+
+});
